e2fe418ccf51220607c9994d2c1082349c377c6b
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-11-17 15:55:50 -0300

    Adiciona função para verificar fim de jogo

--- q7/MateusMatos20241160011q7.c
+++ q7/MateusMatos20241160011q7.c
@@ -2,22 +2,13 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 int main()
 {
-    char tabuleiro[3][3][2] = {'\0'};
-    OpcaoJogador jogadores[2];
-
-    jogadores[0] = obtemOpcaoJogador();
-    if (jogadores[0].valido) {
-        strcpy(tabuleiro[jogadores[0].linha][jogadores[0].coluna], "X");
-        mostraTabuleiro(tabuleiro);
-    } else
-        printf("Eita\n");
+    char tabuleiro[3][3] = {'\0'};
 }
 
-void mostraTabuleiro(char tabuleiro[3][3][2])
+void mostraTabuleiro(char tabuleiro[3][3])
 {
     printf("\tA\tB\tC\n");
     for (int i = 0; i < 3; i++)
@@ -25,7 +16,7 @@ void mostraTabuleiro(char tabuleiro[3][3][2])
         printf("%d\t", i + 1);
 
         for (int j = 0; j < 3; j++)
-            printf("%c\t", tabuleiro[i][j][0]);
+            printf("%c\t", tabuleiro[i][j]);
 
         printf("\n");
     }
@@ -46,12 +37,46 @@ OpcaoJogador obtemOpcaoJogador()
 
     printf("Entrada lida: %s\n", entrada);
 
-    if (('A' <= entrada[0] && entrada[0] <= 'C') && ('1' <= entrada[1] && entrada[1] <= '3')) {
-        opcao.linha = entrada[0] - 'A'; // 'A' é linha 0 no tabuleiro
+    if (('A' <= entrada[0] && entrada[0] <= 'C') &&
+        ('1' <= entrada[1] && entrada[1] <= '3'))
+    {
+        opcao.linha = entrada[0] - 'A';  // 'A' é linha 0 no tabuleiro
         opcao.coluna = entrada[1] - '1'; // '1' é coluna 0 no tabuleiro
         opcao.valido = 1;
-    } else
+    }
+    else
         opcao.valido = 0;
 
     return opcao;
+}
+
+int fimJogo(char tabuleiro[3][3])
+{
+    int tabuleiroCheio = 1;
+    char linha[3], coluna[3];
+
+    // Testa linhas e colunas
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            linha[j] = tabuleiro[i][j];
+            coluna[j] = tabuleiro[j][i];
+
+            if (tabuleiroCheio && !(linha[j] && coluna[j]))
+                tabuleiroCheio = 0; // Atualiza tabuleiro cheio para falso ao encontrar nulo
+        }
+
+        if ((linha[0] && linha[0] == linha[1] && linha[1] == linha[2]) ||
+            (coluna[0] && coluna[0] == coluna[1] && coluna[1] == coluna[2]))
+            return 1;
+    }
+
+    // Testa diagonais
+    if ((tabuleiro[0][0] && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) ||
+        (tabuleiro[2][0] && tabuleiro[2][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[0][2]))
+        return 1;
+
+    // Se nenhum teste passar, jogo encerra se tabuleiro cheio
+    return tabuleiroCheio;
 }
--- q7/MateusMatos20241160011q7.h
+++ q7/MateusMatos20241160011q7.h
@@ -1,13 +1,15 @@
 #ifndef VELHAHEADER
 #define VELHAHEADER
 
-typedef struct OJ{
+typedef struct OJ
+{
     int linha;
     int coluna;
     int valido; // 0: inválido, 1: válido
 } OpcaoJogador;
 
-void mostraTabuleiro(char tabuleiro[3][3][2]);
+void mostraTabuleiro(char tabuleiro[3][3]);
 OpcaoJogador obtemOpcaoJogador();
+int fimJogo(char tabuleiro[3][3]);
 
 #endif // VELHAHEADER
