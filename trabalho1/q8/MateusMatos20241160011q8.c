3ccc95e7c0d95130ca634c31610bf2732799b0b1
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-11-19 22:52:11 -0300

    Q8

--- trabalho1/q8/MateusMatos20241160011q8.c
+++ trabalho1/q8/MateusMatos20241160011q8.c
@@ -1,20 +1,64 @@
 #include "MateusMatos20241160011q8.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-  Jogador j1, j2;
+  Jogador jogadores[2];
+  int tiro;
 
-  setupJogador(&j1);
-  setupJogador(&j2);
+  printf("Montagem dos mapas:\n");
+
+  printf("--Jogador 1--\n");
+  setupJogador(&jogadores[0]);
+  alertaTrocaJogador();
+  printf("--Jogador 2--\n");
+  setupJogador(&jogadores[1]);
+  alertaTrocaJogador();
+
+  int rodada = 1;
+  int iAtual, iAdv;
+  Coordenadas posTiro;
+  do {
+    // Obtem os índices do jogador atual e do seu oponente
+    iAtual = (rodada + 1) % 2;
+    iAdv = (rodada + 2) % 2;
+
+    printf("Vez do jogador %d atirar\n\n", iAtual + 1);
+    do {
+      mostraTabuleiro(ADVERSARIO, jogadores[iAdv].tabuleiro);
+      posTiro = obtemCoordenadas();
+      tiro = atira(posTiro, jogadores[iAdv].tabuleiro);
+
+      if (tiro == TIROCERTO)
+        jogadores[iAdv].posRestantes--;
+
+      if (jogadores[iAdv].posRestantes == 0)
+        break;
+    } while (tiro == TIROINVALIDO || tiro == TIROCERTO);
+
+    mostraTabuleiro(ADVERSARIO, jogadores[iAdv].tabuleiro);
+
+    if (jogadores[iAdv].posRestantes == 0)
+      break;
+    else {
+      sleep(1);
+      printf("\n\n\n");
+      rodada++;
+    }
+  } while ((jogadores[0].posRestantes != 0) &&
+           (jogadores[1].posRestantes != 0));
+
+  // O jogador da última rodada é o vencedor
+  printf("Jogador %d é o vencedor da partida\n", iAtual + 1);
 }
 
 void setupJogador(Jogador *jogador) {
   // Inicializa tabuleiro com posições vazias
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++)
+  for (int i = 0; i < MAXTAB; i++) {
+    for (int j = 0; j < MAXTAB; j++)
       jogador->tabuleiro[i][j] = ' ';
   }
-  mostraTabuleiro(jogador->tabuleiro);
+  mostraTabuleiro(DONO, jogador->tabuleiro);
   // Define índice de navios como -1 (nenhum navio adicionado);
   jogador->iNavios = -1;
   // Posiciona navios
@@ -25,24 +69,36 @@ void setupJogador(Jogador *jogador) {
 
   posicionaNavios(1, 4, jogador);
   posicionaNavios(1, 3, jogador);
-  posicionaNavios(3, 1, jogador);
   if (qtdNavios4 + qtdNavios3 + qtdNavios1 != MAXNAVIOS) {
     qtdNavios2 = MAXNAVIOS - (qtdNavios4 + qtdNavios3 + qtdNavios1);
     posicionaNavios(qtdNavios2, 2, jogador);
   }
+  posicionaNavios(3, 1, jogador);
   // Adiciona total de casas ocupadas pelos navios
   jogador->posRestantes =
       qtdNavios4 * 4 + qtdNavios3 * 3 + qtdNavios2 * 2 + qtdNavios1;
 }
 
-void mostraTabuleiro(char tabuleiro[10][10]) {
-  printf("\t A \t B \t C \t D \t E \t F \t G \t H \t I \t J \n");
+void mostraTabuleiro(int codigoJogador, char tabuleiro[MAXTAB][MAXTAB]) {
+  char simbolo;
+  int i, j;
 
-  for (int i = 0; i < 10; i++) {
+  for (i = 'A'; i < ('A' + MAXTAB); i++) {
+    if (i == 'A')
+      printf("\t");
+    printf(" %c \t", i);
+  }
+  printf("\n");
+  for (i = 0; i < MAXTAB; i++) {
     printf("%d\t", i + 1);
 
-    for (int j = 0; j < 10; j++) {
-      printf("[%c]", tabuleiro[i][j]);
+    for (j = 0; j < MAXTAB; j++) {
+      simbolo = tabuleiro[i][j];
+      // Oculta navios de adversário
+      if (simbolo == 'N' && codigoJogador == ADVERSARIO)
+        simbolo = ' ';
+
+      printf("[%c]", simbolo);
 
       if (j != 9)
         printf("\t");
@@ -52,14 +108,14 @@ void mostraTabuleiro(char tabuleiro[10][10]) {
 }
 
 void marcaTabuleiro(Coordenadas inicio, Coordenadas fim, char simbolo,
-                    char (*tabuleiro)[10]) {
+                    char tabuleiro[MAXTAB][MAXTAB]) {
   for (int i = inicio.linha; i <= fim.linha; i++) {
     for (int j = inicio.coluna; j <= fim.coluna; j++)
       tabuleiro[i][j] = simbolo;
   }
 }
 
-int navioValido(Navio navio, char tabuleiro[10][10]) {
+int navioValido(Navio navio, char tabuleiro[MAXTAB][MAXTAB]) {
   if ((navio.inicio.linha != navio.fim.linha) &&
       (navio.inicio.coluna != navio.fim.coluna)) {
     printf("Inválido! Navio deve estar na vertical ou na horizontal\n");
@@ -68,12 +124,12 @@ int navioValido(Navio navio, char tabuleiro[10][10]) {
 
   // Como navio não está na diagonal, o seu tamanho é obtido pela distância
   // na vertical ou na horizontal
-  int tamanho = 1 + (navio.fim.linha - navio.inicio.linha) +
-                (navio.fim.coluna - navio.inicio.coluna);
-  if (tamanho < 0)
-    tamanho = -tamanho;
+  int distanciaNavio = (navio.fim.linha - navio.inicio.linha) +
+                       (navio.fim.coluna - navio.inicio.coluna);
+  if (distanciaNavio < 0)
+    distanciaNavio = -distanciaNavio;
 
-  if (tamanho != navio.tamanho) {
+  if (distanciaNavio + 1 != navio.tamanho) {
     printf("Inválido! Navio tem tamanho %d\n", navio.tamanho);
     return 0;
   }
@@ -96,13 +152,14 @@ Coordenadas obtemCoordenadas() {
   int linha;
 
   do {
-    printf("Informe coluna e linha (ex.: A1, B10..): ");
+    printf("Informe a posição (ex.: A1, B10..): ");
     fgets(entrada, 10, stdin);
     sscanf(entrada, "%c%d", &coluna, &linha);
 
     if (coluna >= 'a' && coluna <= 'z')
       coluna -= ('a' - 'A');
-  } while ((coluna < 'A' || coluna > 'J') || (linha < 1 || linha > 10));
+  } while ((coluna < 'A' || coluna > 'A' + MAXTAB - 1) ||
+           (linha < 1 || linha > MAXTAB));
 
   Coordenadas coords;
   coords.coluna = coluna - 'A';
@@ -136,9 +193,42 @@ void posicionaNavios(int qtd, int tamNavio, Jogador *jogador) {
         coordsFim = obtemCoordenadas();
         navio.fim = coordsFim;
       }
+
+      // Um jogador malvado pode posicionar o navio de baixo para cima
+      // ou da direita para esquerda
+      if ((navio.inicio.linha > navio.fim.linha) ||
+          (navio.inicio.coluna > navio.fim.coluna)) {
+        Coordenadas temp = navio.inicio;
+        navio.inicio = navio.fim;
+        navio.fim = temp;
+      }
     } while (!navioValido(navio, jogador->tabuleiro));
 
     marcaTabuleiro(navio.inicio, navio.fim, 'N', jogador->tabuleiro);
-    mostraTabuleiro(jogador->tabuleiro);
+    mostraTabuleiro(DONO, jogador->tabuleiro);
   }
+}
+
+int atira(Coordenadas posTiro, char tabuleiro[MAXTAB][MAXTAB]) {
+  switch (tabuleiro[posTiro.linha][posTiro.coluna]) {
+  case 'N': {
+    tabuleiro[posTiro.linha][posTiro.coluna] = 'O';
+    return TIROCERTO;
+  }
+  case ' ': {
+    tabuleiro[posTiro.linha][posTiro.coluna] = 'X';
+    return TIROERRADO;
+  }
+  default: {
+    printf("Tiro inválido! Tente novamente\n");
+    return TIROINVALIDO;
+  }
+  }
+}
+
+void alertaTrocaJogador() {
+  for (int i = 0; i < 250; i++)
+    printf(".\n");
+  printf("Novo jogador! Não vale olhar...\n");
+  sleep(1);
 }
--- trabalho1/q8/MateusMatos20241160011q8.h
+++ trabalho1/q8/MateusMatos20241160011q8.h
@@ -2,6 +2,14 @@
 #define NAVALHEADER
 
 #define MAXNAVIOS 5
+#define MAXTAB 10
+
+#define DONO 1
+#define ADVERSARIO 2
+
+#define TIROCERTO 1
+#define TIROERRADO 2
+#define TIROINVALIDO 3
 
 typedef struct Coordenadas {
   int linha;
@@ -18,16 +26,21 @@ typedef struct Navio {
 typedef struct Jogador {
   Navio navios[MAXNAVIOS];
   int iNavios;
-  char tabuleiro[10][10];
+  char tabuleiro[MAXTAB][MAXTAB];
   int posRestantes;
 } Jogador;
 
-void mostraTabuleiro(char tabuleiro[10][10]);
+// O tabuleiro é exibido de forma diferente se
+// jogador for dono dele ou não (codigoJogador)
+void mostraTabuleiro(int codigoJogador, char tabuleiro[MAXTAB][MAXTAB]);
 void marcaTabuleiro(Coordenadas inicio, Coordenadas fim, char simbolo,
-                    char tabuleiro[10][10]);
+                    char tabuleiro[MAXTAB][MAXTAB]);
 void setupJogador(Jogador *jogador);
-int navioValido(Navio navio, char tabuleiro[10][10]);
+int navioValido(Navio navio, char tabuleiro[MAXTAB][MAXTAB]);
 Coordenadas obtemCoordenadas();
 void posicionaNavios(int qtd, int tamNavio, Jogador *jogador);
-
+// A função atirar altera o tabuleiro adversário
+// e retorna TIROCERTO, TIROERRADO ou TIROINVALIDO
+int atira(Coordenadas posTiro, char tabuleiro[MAXTAB][MAXTAB]);
+void alertaTrocaJogador();
 #endif // NAVALHEADER
